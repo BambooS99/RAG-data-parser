82a84c8dd5f6304c59d57645322acd80593caf21
BambooS99 <owaishassan786@gmail.com>
2026-07-06 18:06:38 -0400

    fixed typo

--- src/components/chatBubble/chatBubble.tsx
+++ src/components/chatBubble/chatBubble.tsx
@@ -3,7 +3,7 @@ import "./chatBubble.scss";
 export default function ChatBubble({ text }: { text: string }) {
   return (
     <>
-      <div className={'"chat-bubble${isMe ? "me": "them"}'}>{text}</div>
+      <div className={'chat-bubble${isMe ? "me": "them"}'}>{text}</div>
     </>
   );
 }
